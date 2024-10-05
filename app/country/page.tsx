5fff4b75d50a212e70688d8dc31273ad6dae5330
unknown <pranavram227@gmail.com>
2024-10-05 21:30:10 +0530

    1.5

--- app/country/page.tsx
+++ app/country/page.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from 'react';
 import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
 import { Bar } from 'react-chartjs-2';
 import Papa from 'papaparse';
-import styles from './Country.module.css'; // Adjust path if necessary
+import styles from './country.module.css'; // Adjust path if necessary
 
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
